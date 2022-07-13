4b2b6e108f342ebdd524e06f5e968524f1ea1392
Dentonu <108883101+Dentonu@users.noreply.github.com>
2022-07-13 14:00:11 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -29,7 +29,6 @@ async function bootstrap() {
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
     .addBearerAuth()
- 
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
